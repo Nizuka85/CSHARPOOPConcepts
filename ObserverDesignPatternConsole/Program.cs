5e6a48c8f822611bd63d084cee5f3151596c7a51
Kelanni Path | New C# .Net Web Dev <kelannian@gmail.com>
2024-06-05 00:52:00 +0100

    Observer
    forma de marcar eventos para algum fim; no caso notificar alteração de preços

---
+++ ObserverDesignPatternConsole/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Net.Http.Headers;
+
+namespace ObserverDesignPatternConsole
+{      
+    interface IMarket
+    {
+        void Update(Product product);
+    }
+    abstract class Product
+    {
+        private double price;
+        List<IMarket> Markets= new List<IMarket>();
+        public Product(double _price)
+        {
+            price = _price;
+        }
+        public void Attach(IMarket market)
+        {
+            Markets.Add(market);
+        }
+        public void Deattech(IMarket market)
+        {
+            Markets.Remove(market);
+        }
+        public void Notify()
+        {
+            foreach (IMarket market in Markets)
+            {
+                market.Update(this);
+            }
+            Console.WriteLine("");
+        }
+        public double priceperpound
+        {
+            get { return price; }
+            set
+            {
+                if (price != value)
+                    price = value;
+                Notify();
+            }
+        }
+
+    }
+    class Chocolate : Product
+    {
+        public Chocolate(double price) : base(price) { }
+    }
+    class Market : IMarket
+    {
+        private string Name;
+        private double price;
+        public Market(string _Name, double _price)
+        {
+            Name = _Name;
+            price = _price;
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine("Notify: In "+ Name + "the price of "+ product.GetType().Name+ " was changed with "+ product.priceperpound);
+        }
+    }
+     class Program
+    {   
+        static void Main(string[] args)
+        {
+            Chocolate chocolate = new Chocolate(2);
+            var qlqrcvoisa = new Market("Market1", 1);
+            chocolate.Attach(qlqrcvoisa);
+            chocolate.Attach(new Market("Market2", 2));
+            chocolate.Attach(new Market("Market3", 3));
+            chocolate.Attach(new Market("Market4", 4));
+
+            chocolate.priceperpound = 5;
+            chocolate.priceperpound = 6;
+            chocolate.priceperpound = 7;
+            chocolate.priceperpound = 8;
+
+            chocolate.Deattech(qlqrcvoisa);
+            chocolate.priceperpound = 12;
+            Console.ReadKey();
+        }
+    }
+}
