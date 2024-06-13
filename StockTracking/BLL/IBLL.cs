5bdf7a39e87644fab17733890ce18df12d6653d5
Kelanni Path | New C# .Net Web Dev <kelannian@gmail.com>
2024-06-14 00:28:22 +0100

    project structure OOP
    usando ADO.NET que auto completa o começo do (parâmetros iniciais)  baseado no banco de dados

---
+++ StockTracking/BLL/IBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.BLL
+{
+    interface IBLL<T,K> where T : class where K : class
+    {
+        bool Insert(T item);
+        bool Update(T item);
+        bool Delete(T item);
+        K Select();
+        bool GetBack(T entity);
+    }
+}
---
+++ StockTracking/DAL/DAO/IDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTracking.DAL.DAO
+{
+    interface IDAO<T,K> where T : class where K : class
+    {
+        List<K> Select();
+        bool Insert (T item);
+        bool Update (T item);   
+        bool Delete (T item);
+        bool GetBack(int ID);
+
+    }
+}
